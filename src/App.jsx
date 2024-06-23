fea5cddb8c87d5a7c3fe4fd8225730261107a9f0
Piyush Vishwakarma <piyushvishwakarma6706@gmail.com>
2024-06-23 14:00:34 +0530

    debug

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,44 @@
+import Nav from "./Components/nav";
+import Landingpg from "./Pages/Home/landingpage";
+import OrderPrice from "./Pages/Orders/OrderPrice";
 
-import Nav from './Components/nav'
-import Landingpg from './Pages/Home/landingpage'
-import OrderPrice from './Pages/Orders/OrderPrice'
+import { onAuthStateChanged, getAuth } from "firebase/auth";
 
-import { onAuthStateChanged, getAuth } from 'firebase/auth'
+import { useEffect, useState } from "react";
 
-import { useEffect, useState } from 'react'
+import { app } from "./Firebase/firbase";
 
-import { app } from './Firebase/firbase'
+const auth = getAuth(app);
 
-
-const auth= getAuth(app)
-
-export let isUserlogged= false;
+export let isUserlogged = false;
 
 function App() {
+  const [user, setuser] = useState(null);
+  const [uDetails, setuDetails] = useState({ name: null, image: null });
 
-  const [user, setuser]= useState(null);
-  const[uDetails,setuDetails]= useState({name:null, image:null});
-
-  useEffect(()=>{
-      onAuthStateChanged(auth, (user)=>{
-          if(user){
-            isUserlogged= true
-            setuser(user)
-            setuDetails({...uDetails, name: user.displayName, image: user.photoURL})
-          }
-      })
-  },[])
-
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        isUserlogged = true;
+        setuser(user);
+        setuDetails({
+          ...uDetails,
+          name: user.displayName,
+          image: user.photoURL,
+        });
+      }
+    });
+  }, []);
 
   return (
     <>
-    <div className=' relative'>
-      <Nav profile={uDetails.image} ></Nav>
-    <Landingpg></Landingpg>
-    </div>
-    <OrderPrice />
-    
+      <div className=" relative">
+        <Nav profile={uDetails.image}></Nav>
+        <Landingpg></Landingpg>
+      </div>
+      <OrderPrice />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
