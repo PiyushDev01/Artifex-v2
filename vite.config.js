3d359ba9d7bde79530d449cd18f9136ca5eede73
Piyush <piyushvishwakarma6707@gmail.com>
2024-12-07 14:56:34 +0530

    added vercel json and updated vite conf

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: '/', // Ensure this matches your deployment base path
 })
